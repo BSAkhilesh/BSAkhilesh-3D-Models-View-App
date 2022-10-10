e1480ff5353e2b0381ee923bcfaf419bc0ab9487
BS Akhilesh <114279315+BSAkhilesh@users.noreply.github.com>
2022-10-10 23:03:44 +0530

    Update urls.py

--- Project_file/urls.py
+++ Project_file/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
      path('', include('App_file.urls')),
@@ -25,3 +28,4 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
      
 ]
++static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
